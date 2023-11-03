e4bfb711f4fbf8d685e20d5b1039d08dc317cd77
Hashan Ranasinghe <146754994+HashanApprit@users.noreply.github.com>
2023-11-04 00:43:22 +0530

    last

--- models/User.js
+++ models/User.js
@@ -1,30 +1,29 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        require: true,
-        min: 3,
-        man: 20,
-        unique: true,
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+
+      unique: true,
     },
-    email:{
-        type: String,
-        require: true,
-        max: 50,
-        unique: true,
+    email: {
+      type: String,
+      require: true,
+
+      unique: true,
     },
-    password:{
-        type: String,
-        require: true,
-        min: 6,
+    password: {
+      type: String,
+      require: true,
     },
-    profilePicture:{
-        type: String,
-        default: "",
+    profilePicture: {
+      type: String,
+      default: "",
     },
-}, 
-{timestamps:true}
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
